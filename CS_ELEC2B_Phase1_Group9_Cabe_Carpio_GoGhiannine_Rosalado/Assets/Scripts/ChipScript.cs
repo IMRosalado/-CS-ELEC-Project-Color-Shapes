d143c832b8111a0a67a456fb8792568f13a999c0
Isabela Marie Rosalado <Isabela Marie Rosalado>
2016-09-26 16:24:54 +0800

    no message

---
+++ CS_ELEC2B_Phase1_Group9_Cabe_Carpio_GoGhiannine_Rosalado/Assets/Scripts/ChipScript.cs
@@ -0,0 +1,35 @@
+﻿/*
+*Chip class
+*contains all information about the chip;
+*/
+
+using UnityEngine;
+using System.Collections;
+
+public class ChipScript : MonoBehaviour {
+
+	public string chipColor = "none";
+	public Sprite red;
+	public Sprite blue;
+
+	private SpriteRenderer rend;
+
+	void Start () {
+		rend = GetComponent<SpriteRenderer> ();
+	}
+	/*
+		Shows the chip with corresponding color
+	*/
+	public void ShowChip(string color){
+		chipColor = color;
+		if (chipColor == "red") {
+			rend.sprite = red;
+		}
+		else if (chipColor == "blue"){
+			rend.sprite = blue;
+		}
+		rend.enabled = true;
+
+	}
+
+}
---
+++ CS_ELEC2B_Phase1_Group9_Cabe_Carpio_GoGhiannine_Rosalado/Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * GameManagerScript 
+*/
+
+using UnityEngine;
+using System.Collections;
+
+public class GameManagerScript : MonoBehaviour {
+
+	public int gridSize=9;
+	public GameObject chip;
+	public GameObject[] chips;
+
+
+	private bool isPlayerOne = true;
+	private int[,] grid;
+
+	void Start () {
+		grid = new int[gridSize, gridSize];
+	}
+
+	void Update () {
+
+		//checks on mouse click
+		if (Input.GetMouseButtonDown (0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit2D rayHit = Physics2D.Raycast(ray.origin,ray.direction,Mathf.Infinity);
+			GameObject clicked;
+			if (rayHit) {
+				clicked = rayHit.collider.gameObject;
+				ShowChip (clicked);
+			}
+		}
+	}
+
+	/*
+		displays chip on board
+	*/
+	void ShowChip(GameObject clicked){
+		int i = System.Array.IndexOf (chips, clicked);
+		if (i > -1) {
+			int row = (int)(i / gridSize);
+			int col = i % gridSize;
+			grid [row,col] = (isPlayerOne)? 1:2; //fills the grid with integer values for checking after the game
+
+			if (isPlayerOne) {
+				clicked.GetComponent<ChipScript> ().ShowChip("red");
+			} else {
+				clicked.GetComponent<ChipScript> ().ShowChip("blue");
+			}
+
+			isPlayerOne = !isPlayerOne; 
+		}
+
+	}
+}
